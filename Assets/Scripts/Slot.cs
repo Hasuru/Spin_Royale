e1b21168af42aab32b3211c3d758b06aad46b2a7
Hasuru <hugo.as.silva25@gmail.com>
2024-02-27 13:26:21 +0000

    simple spin animation implementation

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,19 +1,71 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
+    [Header("Sprite Information")]
     [SerializeField] private Sprite[] sprites;
     [SerializeField] public Image currentSprite;
 
+    [Header("Animation Info")]
+    [SerializeField] private float timeInterval;
+
+    private bool slotStopped;
+    private int spriteIndex;
+
+    public void Start()
+    {
+        slotStopped = true;
+    }
+
+    public void Update()
+    {
+        if (slotStopped)
+        {
+            if (currentSprite.sprite != sprites[spriteIndex]) currentSprite.sprite = sprites[spriteIndex];
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+        } 
+        else if (!slotStopped)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
     public int RunSlot()
     {
-        int value = Random.Range(0, sprites.Length);
+        spriteIndex = Random.Range(0, sprites.Length);
 
-        currentSprite.sprite = sprites[value];
+        StartCoroutine(Spin());
 
-        return value;
+        return spriteIndex;
+    }
+
+    private IEnumerator Spin()
+    {
+        slotStopped = false;
+        float animTimer = 0f;
+
+        while (animTimer < timeInterval)
+        {
+            int value = Random.Range(0, sprites.Length);
+            currentSprite.sprite = sprites[value];
+            animTimer += Time.deltaTime;
+
+            Debug.Log(animTimer + "/" + timeInterval);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        slotStopped = true;
     }
 }
